2f08dee0c5799dcf24b01bb0520e1b8bf7764804
Steve <15229911+stevehanstudio@users.noreply.github.com>
2022-07-05 18:39:55 -0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+const multer = require('multer')
 var cors = require('cors');
 require('dotenv').config()
+
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,8 +14,15 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// {
+//   name: "Selfie_20170531_204132.jpg",
+//   type: "image/jpeg",
+//   size: 108349
+// }
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.file
+  res.json({ name, type, size })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
